64405e8b638c2a9b13f21b60cbbe011e6eccdec6
Ayman <anang.ayman@gmail.com>
2025-05-08 14:25:05 +0700

    Fixed rocket size, fixed initials

--- tertiary.py
+++ tertiary.py
@@ -1,7 +1,9 @@
 def get_initials(full_name):
     name_parts = full_name.split()
     first_name = name_parts[0]
-    # Ensure there is a last name
-    last_name = name_parts[-1] if len(name_parts) > 1 else ''
-    initials = (first_name[0] + last_name[0]).upper()  # DJ for "Diven Dechal Jatiputra"
+    
+    # If there's no last name, use an empty string for the initial
+    last_initial = name_parts[1][0] if len(name_parts) > 1 else ''
+    
+    initials = (first_name[0] + last_initial).upper()  # DJ for "Diven Dechal Jatiputra"
     return first_name, initials
