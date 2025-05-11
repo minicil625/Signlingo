19f28452768f3649c1c8cc8d89717a4aed0490b4
AnangAyman <110002440+AnangAyman@users.noreply.github.com>
2025-05-11 14:14:29 +0700

    Delete static/gamescript.js
    Old gamescript

--- static/gamescript.js
+++
@@ -1,7 +0,0 @@
-function showCorrectAnswer() {
-  document.querySelector('.correct-answer').style.display = 'block';
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector('.option.o').addEventListener('click', showCorrectAnswer);
-});
